a0a9d62a2b5c89b6dbc5f5cba860580c0b2720f2
Vicente Yuen <ruanweibiao@modernavenue.com>
2016-10-18 15:48:54 +0800

    add index

---
+++ sche-job-search-index/src/main/java/org/vsg/rmodel/schejob/search/image/FullIndexScheduler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vsg.rmodel.schejob.search.image;
+
+/**
+ * @author ruanweibiao
+ *
+ */
+public class FullIndexScheduler {
+	
+	// --- set the schedule --
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+
+}
---
+++ sche-job-search-index/src/main/java/org/vsg/rmodel/schejob/search/image/JobRunModule.java
@@ -0,0 +1,13 @@
+package org.vsg.rmodel.schejob.search.image;
+
+import com.google.inject.AbstractModule;
+
+public class JobRunModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
